fd48a725ffcd802cbcaba4dccd8f346d1cc78507
Rakshitha <>
2022-03-07 16:12:41 +0530

    Added initial setup.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MoEngageGeofence",
+    products: [
+        .library(
+            name: "MoEngageGeofence",
+            targets: ["MoEngageGeofenceSPM","MoEngageGeofence"]),
+    ],
+    dependencies: [
+        .package(name: "MoEngage-iOS-SDK",url: "https://github.com/moengage/MoEngage-iOS-SDK.git", from: "8.0.0"),
+    ],
+    targets: [
+        .target(name: "MoEngageGeofenceSPM", dependencies: ["MoEngage-iOS-SDK"], path: "Sources/..",exclude: ["MOGeofence.podspec", "README.md","./Frameworks/MoEngageGeofence.xcframework", "LICENSE","CHANGELOG.md"]),
+        .binaryTarget(name: "MoEngageGeofence",path: "./Frameworks/MoEngageGeofence.xcframework")
+    ]
+)
